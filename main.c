e8e145642b88c0d52a82c6470c53f21d9299cace
0xshr00msz <yldeviermagpusao@gmail.com>
2023-12-26 20:40:52 +0800

    Created a struct for the admin

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Structures for Admin
+typedef struct Admin{
+    char eventName[50];
+    char eventAddress[50];
+    int month, day, year;       // Format: MM/DD/YYYY
+    // Time Variable
+} Admin;
+
 int main(){
     
 }
