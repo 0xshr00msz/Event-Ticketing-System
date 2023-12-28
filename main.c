954cffb7f6fd2cb887f376747e125872ca98ee8b
0xshr00msz <yldeviermagpusao@gmail.com>
2023-12-28 13:36:33 +0800

    added function for listing the events

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<dirent.h>
+#include<string.h>
 
 // Structures for Admin
 typedef struct Admin{
@@ -9,6 +11,49 @@ typedef struct Admin{
     // Time Variable
 } Admin;
 
-int main(){
-    
+// Structure for User
+typedef struct User{
+    char name[50];
+    int age;
+    char sex;
+    char address[100];
+    char mobileNumber[12];   // 11 digit number + 1 for null terminator - 09xxxxxxxxx
+    char emailAddress[50];
+} User;
+
+// Display Events listed inside the events directory
+int displayEvents(){
+    char path[50] = "events/";
+
+    printf("=====================================================\n");
+    printf("\nWelcome to XY Event Reservation and Ticketing System\n");
+    printf("\n=====================================================\n");
+    printf("Listed below are the events\n\n");
+
+    DIR *dir = opendir(path);
+    if(dir == NULL){
+        perror("Error opening directory");
+        return EXIT_FAILURE;
+    }
+
+    struct dirent *entry;
+    int i = 0;
+    while((entry = readdir(dir)) != NULL){
+        if(strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0){
+            if(entry->d_type == DT_DIR){
+                printf("%d\t%s\n", i, entry->d_name);
+            }
+            i++;
+        }
+    }
+    closedir(dir);
+    return EXIT_SUCCESS;
 }
+
+int main(){
+
+    // Display Events listed inside the events directory
+    displayEvents();
+
+}
+
