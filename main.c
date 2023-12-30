f41a023af28c89544549e93e6f50a3f0e654c2e8
0xshr00msz <yldeviermagpusao@gmail.com>
2023-12-31 06:10:07 +0800

    Implemented Directory Traversals: Unfinished

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<dirent.h>
+#include<direct.h>
 #include<string.h>
 
 // Structures for Admin
@@ -21,9 +22,59 @@ typedef struct User{
     char emailAddress[50];
 } User;
 
-// Display Events listed inside the events directory
-int displayEvents(){
-    char path[50] = "events/";
+
+int selectEvent(int index){
+    int choice;
+    printf("Select an event: ");
+    scanf("%d", &choice);
+
+    if(choice < 0 || choice > index){
+        printf("\nInvalid choice. Please try again.\n");
+        return selectEvent(index);
+    }
+    return choice;
+}
+
+int goToEvent(int choice, int index) {
+    char buffer[1024];
+
+    char *directory = "events/";
+    _chdir(directory);
+
+    DIR *dir;
+    struct dirent *entry;
+
+    if((dir = opendir(".")) != NULL){
+        int i = 1;
+        while ((entry = readdir(dir)) != NULL) {
+            // Process each entry
+            if (entry->d_type == DT_DIR) {
+                // If the entry is a directory, change the working directory
+                if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+                    if(i == choice){
+                         printf("Changing working directory to: %s\n", entry->d_name);
+                        _chdir(entry->d_name);
+                        break;
+                    }
+                   i++;
+                }
+            }
+        }
+        closedir(dir);
+
+        if (_getcwd(buffer, 1024) == NULL) {
+            printf("Get current working directory failed!\n");
+            return 1;
+        }
+        printf("\nCurrent directory: \n%s\n\n", buffer);
+    } else {
+        perror("Unable to open directory\n");
+        return 1;
+    }
+}
+
+void userMenu(){
+    char path[25] = "events/";  // Path to events directory
 
     printf("=====================================================\n");
     printf("\nWelcome to XY Event Reservation and Ticketing System\n");
@@ -32,28 +83,30 @@ int displayEvents(){
 
     DIR *dir = opendir(path);
     if(dir == NULL){
-        perror("Error opening directory");
-        return EXIT_FAILURE;
+        printf("Error opening directory");
+        exit(1);
     }
 
     struct dirent *entry;
-    int i = 0;
+    int index = 1;
     while((entry = readdir(dir)) != NULL){
         if(strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0){
             if(entry->d_type == DT_DIR){
-                printf("%d\t%s\n", i, entry->d_name);
+                printf("%d\t%s\n", index, entry->d_name);
             }
-            i++;
+            index++;
         }
     }
+    printf("%d\tExit\n\n", 0);
     closedir(dir);
-    return EXIT_SUCCESS;
+
+    int choice = selectEvent(index - 1);
+
+    goToEvent(choice, index - 1);
+
 }
 
 int main(){
-
-    // Display Events listed inside the events directory
-    displayEvents();
-
+    userMenu();
 }
 
