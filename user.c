7d2e41d230ed9913d2609142a978f69546ae4631
0xshr00msz <yldeviermagpusao@gmail.com>
2024-01-06 16:51:35 +0800

    added user input for user information

--- user.c
+++ user.c
@@ -38,14 +38,85 @@ void displayEvents(){
             }
         }
     }
-    printf("\n");
+    printf("0\tExit\n\n");
     closedir(dir);
 }
 
+int selectEvent(){
+    int eventNum;
+    printf("Select an event: ");
+    scanf("%d", &eventNum);
+    return eventNum;
+}
+
+void appendToFile(char *fileName, User *userDetails){
+    FILE *file = fopen(fileName, "a+");
+    if(file == NULL){
+        printf("Unable to open file!");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(file, "\n%s,%d,%c,%s,%s,%s", 
+        userDetails->name,
+        userDetails->age,
+        userDetails->sex,
+        userDetails->address,
+        userDetails->mobileNum,
+        userDetails->emailAddr
+    );
+}
+
+void inputUserDetails(int choice){
+    
+    if(choice != 0){
+        char fileName[50];
+        DIR *dir = opendir(workingDir);
+        struct dirent *entry;
+        int i = 1;
+
+        while((entry = readdir(dir)) != NULL){
+            if(entry->d_type == DT_REG){
+                const char *dot = strrchr(entry->d_name, '.');
+                if(i == choice){
+                    snprintf(fileName, sizeof(fileName), "%s/%s", workingDir, entry->d_name);
+                    break;
+                }
+                i++;
+            }
+        }
+        closedir(dir);
+
+        User newUser;
+
+        printf("Please enter your information:\n");
+        printf("Name: ");
+        scanf(" %[^\n]", newUser.name);
+        printf("Age: ");
+        scanf("%d", &newUser.age);
+        printf("Sex (M/F): ");
+        scanf(" %c", &newUser.sex);
+        printf("Address: ");
+        scanf(" %[^\n]", newUser.address);
+        printf("Mobile Number: ");
+        scanf("%s", newUser.mobileNum);
+        printf("Email Address: ");
+        scanf("%s", newUser.emailAddr);
+
+        // Append user data to the selected file
+        appendToFile(fileName, &newUser);
+    }
+    
+}
+
 void userMenu(){
+    int flag = 1;
+    int choice;
+
     displayEvents();
+    choice = selectEvent();
+    inputUserDetails(choice);
 }
 
 int main(){
-    displayEvents();
+    userMenu();
 }
